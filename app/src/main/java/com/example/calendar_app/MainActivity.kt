be3499f618dc0fbdf44ddf7dd2a8d80e895a7210
Alec <aarnaoutoglou@roc-dev.com>
2024-01-23 15:34:41 +0100

    final commit

--- app/src/main/java/com/example/calendar_app/MainActivity.kt
+++ app/src/main/java/com/example/calendar_app/MainActivity.kt
@@ -12,35 +12,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calendar_app.ui.theme.CalendarappTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    var year1: Int = 0
+    var month1: Int = 0
+    var day1: Int = 0
+
+    var year2: Int = 0
+    var month2: Int = 0
+    var day2: Int = 0
+
+    var total1 : Int = 0
+    var total2 : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CalendarappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        calendarView.setOnDateChangeListener {
+                calendarView, i, i1, i2 ->
+            year1 = i
+            month1 = i1+1
+            day1 = i2
+        }
+        calendarView2.setOnDateChangeListener {
+                calendarView, i, i1, i2 ->
+            year2 = i
+            month2 = i1+1
+            day2 = i2
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun calculate(view: View) {
+        total1 = (year1*360) + (month1*30) + day1
+        total2 = (year2*360) + (month2*30) + day2
+        println(total1)
+        println(total2)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalendarappTheme {
-        Greeting("Android")
+        textView.text = "${total1-total2}" + " day"
     }
 }
